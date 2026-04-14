de00eeaec2f7c715677f97ad7f28b3abef552e01
Priyangshu Das <priyangshud924@gmail.com>
2026-04-14 23:33:14 +0530

    modified reverse problem cpp

--- 02_BasicMaths/02_reversenum.cpp
+++ 02_BasicMaths/02_reversenum.cpp
@@ -8,9 +8,15 @@ void reversenum(int n)
     long long revnum = 0;
 
     // Extract digits and build reversed number
-    while(n > 0)
+    while(n != 0)
     {
-        lastdigit = n % 10;               // get last digit
+        lastdigit = n % 10;// get last digit
+         // Overflow check BEFORE updating
+            if (revnum > INT_MAX/10 || revnum < INT_MIN/10)
+            {
+                cout << "The reverse of the number is: 0";
+            }
+                            
         revnum = (revnum * 10) + lastdigit; // append digit
         n = n / 10;                      // remove last digit
     }
