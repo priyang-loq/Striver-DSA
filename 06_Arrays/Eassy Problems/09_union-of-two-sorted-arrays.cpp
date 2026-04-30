d714279e7e9ba010a3d07b0197f56859ec248fad
Priyangshu Das <priyangshud924@gmail.com>
2026-04-30 21:21:41 +0530

    Refactored findUnion function to use two-pointer technique for improved efficiency and updated main function for dynamic input handling.

--- 06_Arrays/Eassy Problems/09_union-of-two-sorted-arrays.cpp
+++ 06_Arrays/Eassy Problems/09_union-of-two-sorted-arrays.cpp
@@ -1,47 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Define the Solution class
+/*
+    Class: Solution
+    Function: findUnion
+    Purpose: Finds the union of two sorted arrays using two-pointer technique
+*/
 class Solution {
 public:
-    // Function to find union of two arrays
-    vector<int> FindUnion(int arr1[], int arr2[], int n, int m) {
-        // Create a map to store frequency of each element
-        map<int, int> freq;
-        // Create a vector to store the union result
-        vector<int> Union;
-        // Loop through first array and store frequency
-        for (int i = 0; i < n; i++)
-            freq[arr1[i]]++;
-        // Loop through second array and store frequency
-        for (int i = 0; i < m; i++)
-            freq[arr2[i]]++;
-        // Traverse the map to get unique sorted elements
-        for (auto &it : freq)
-            Union.push_back(it.first);
-        // Return the union vector
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m) {
+        vector<int> Union;  // Stores final union
+
+        int i = 0, j = 0;   // Two pointers
+
+        // Traverse both arrays
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                // Avoid duplicates
+                if (Union.empty() || Union.back() != arr1[i])
+                    Union.push_back(arr1[i]);
+                i++;
+            }
+            else if (arr2[j] < arr1[i]) {
+                if (Union.empty() || Union.back() != arr2[j])
+                    Union.push_back(arr2[j]);
+                j++;
+            }
+            else {
+                // If both are equal
+                if (Union.empty() || Union.back() != arr1[i])
+                    Union.push_back(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        // Remaining elements of arr1
+        while (i < n) {
+            if (Union.empty() || Union.back() != arr1[i])
+                Union.push_back(arr1[i]);
+            i++;
+        }
+
+        // Remaining elements of arr2
+        while (j < m) {
+            if (Union.empty() || Union.back() != arr2[j])
+                Union.push_back(arr2[j]);
+            j++;
+        }
+
         return Union;
     }
 };
 
 int main() {
-    // Define size of first array
-    int n = 10;
-    // Define size of second array
-    int m = 7;
-    // Initialize first array
-    int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    // Initialize second array
-    int arr2[] = {2, 3, 4, 4, 5, 11, 12};
-    // Create object of Solution class
+    int n, m;
+
+    // Input size of first array
+    cout << "Enter size of first array: ";
+    cin >> n;
+    int arr1[n];
+
+    // Input elements of first array (sorted)
+    cout << "Enter elements of first array (sorted): ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+
+    // Input size of second array
+    cout << "Enter size of second array: ";
+    cin >> m;
+    int arr2[m];
+
+    // Input elements of second array (sorted)
+    cout << "Enter elements of second array (sorted): ";
+    for (int i = 0; i < m; i++) {
+        cin >> arr2[i];
+    }
+
+    // Create object
     Solution obj;
-    // Call FindUnion function
-    vector<int> Union = obj.FindUnion(arr1, arr2, n, m);
-    // Print output message
-    cout << "Union of arr1 and arr2 is " << endl;
-    // Print all elements of union
-    for (auto &val : Union)
+
+    // Get union result
+    vector<int> result = obj.findUnion(arr1, arr2, n, m);
+
+    // Output result
+    cout << "Union of the two arrays is: ";
+    for (int val : result) {
         cout << val << " ";
-    // Return success
+    }
+    cout << endl;
+
     return 0;
 }
