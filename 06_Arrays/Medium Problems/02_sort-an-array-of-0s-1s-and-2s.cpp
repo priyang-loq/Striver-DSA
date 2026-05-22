fb586bc4ec0ebf1fe7d7f0e0a163fc350005a0ba
Priyangshu Das <priyangshud924@gmail.com>
2026-05-22 22:43:56 +0530

    Refactor sortZeroOneTwo function to implement counting sort for improved clarity and efficiency in sorting an array of 0s, 1s, and 2s.

--- 06_Arrays/Medium Problems/02_sort-an-array-of-0s-1s-and-2s.cpp
+++ 06_Arrays/Medium Problems/02_sort-an-array-of-0s-1s-and-2s.cpp
@@ -3,41 +3,48 @@ using namespace std;
 
 class Solution {
 public:
-    // Function to sort array containing 0s, 1s, and 2s using Dutch National Flag Algorithm
+    // Function to sort the array containing only 0s, 1s and 2s
     void sortZeroOneTwo(vector<int>& nums) {
-        // Initialize three pointers: low, mid starting from 0, high from end of array
-        int low = 0, mid = 0, high = nums.size() - 1;
+        // Initialize count variables for 0s, 1s, and 2s
+        int count0 = 0, count1 = 0, count2 = 0;
 
-        // Process elements until mid pointer crosses high pointer
-        while (mid <= high) {
-            // If current element is 0, swap with low and move both pointers forward
-            if (nums[mid] == 0) {
-                swap(nums[mid], nums[low]);
-                mid++;
-                low++;
-            }
-            // If current element is 1, it's already in correct place → move mid forward
-            else if (nums[mid] == 1) {
-                mid++;
-            }
-            // If current element is 2, swap with high and move only high pointer backward
-            else {
-                swap(nums[mid], nums[high]);
-                high--;
-            }
+        // Count the frequency of 0s, 1s, and 2s in the array
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == 0) count0++;
+            else if(nums[i] == 1) count1++;
+            else count2++;
+        }
+
+        // Overwrite the array with the counted values
+        int index = 0;
+
+        // Fill with 0s
+        while(count0--) {
+            nums[index++] = 0;
+        }
+
+        // Fill with 1s
+        while(count1--) {
+            nums[index++] = 1;
+        }
+
+        // Fill with 2s
+        while(count2--) {
+            nums[index++] = 2;
         }
     }
 };
 
 // Driver code
 int main() {
-    Solution obj;
-    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    vector<int> nums = {1, 0, 2, 1, 0};
 
+    Solution obj;
     obj.sortZeroOneTwo(nums);
 
-    for (int val : nums)
-        cout << val << " ";
+    for(int x : nums) {
+        cout << x << " ";
+    }
 
     return 0;
 }
