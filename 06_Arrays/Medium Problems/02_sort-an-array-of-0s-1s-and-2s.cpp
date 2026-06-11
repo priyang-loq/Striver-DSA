f05d04f67f8f925e322bc48cc78000d27904df29
Priyangshu Das <priyangshud924@gmail.com>
2026-06-11 23:01:34 +0530

    Refactor sortZeroOneTwo function to implement Dutch National Flag Algorithm for improved efficiency in sorting an array of 0s, 1s, and 2s

--- 06_Arrays/Medium Problems/02_sort-an-array-of-0s-1s-and-2s.cpp
+++ 06_Arrays/Medium Problems/02_sort-an-array-of-0s-1s-and-2s.cpp
@@ -3,49 +3,41 @@ using namespace std;
 
 class Solution {
 public:
-    // Function to sort an array containing only 0s, 1s, and 2s
+    // Function to sort array containing 0s, 1s, and 2s using Dutch National Flag Algorithm
     void sortZeroOneTwo(vector<int>& nums) {
-        // Count of 0s, 1s, and 2s
-        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        // Initialize three pointers: low, mid starting from 0, high from end of array
+        int low = 0, mid = 0, high = nums.size() - 1;
 
-        // First pass: Count the number of 0s, 1s, and 2s
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) cnt0++;
-            else if (nums[i] == 1) cnt1++;
-            else cnt2++;
-        }
-
-        // Second pass: Fill the array with 0s, then 1s, then 2s
-
-        // Fill the first 'cnt0' elements with 0
-        for (int i = 0; i < cnt0; i++) {
-            nums[i] = 0;
-        }
-
-        // Fill the next 'cnt1' elements with 1
-        for (int i = cnt0; i < cnt0 + cnt1; i++) {
-            nums[i] = 1;
-        }
-
-        // Fill the remaining elements with 2
-        for (int i = cnt0 + cnt1; i < nums.size(); i++) {
-            nums[i] = 2;
+        // Process elements until mid pointer crosses high pointer
+        while (mid <= high) {
+            // If current element is 0, swap with low and move both pointers forward
+            if (nums[mid] == 0) {
+                swap(nums[mid], nums[low]);
+                mid++;
+                low++;
+            }
+            // If current element is 1, it's already in correct place → move mid forward
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            // If current element is 2, swap with high and move only high pointer backward
+            else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
         }
     }
 };
 
 // Driver code
 int main() {
-    vector<int> nums = {0, 2, 1, 2, 0, 1};
+    Solution obj;
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
 
-    Solution sol;
-    sol.sortZeroOneTwo(nums);
+    obj.sortZeroOneTwo(nums);
 
-    cout << "After sorting:" << endl;
-    for (int i = 0; i < nums.size(); i++) {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
+    for (int val : nums)
+        cout << val << " ";
 
     return 0;
 }
