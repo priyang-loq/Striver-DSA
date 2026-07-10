f8ddd6852695ee6daab75b6fb9dcfef2d87b7cd5
Priyangshu Das <priyangshud924@gmail.com>
2026-07-10 15:42:11 +0530

    Refactor leaders function to improve efficiency and clarity

--- 06_Arrays/Medium Problems/09_leaders-in-an-array.cpp
+++ 06_Arrays/Medium Problems/09_leaders-in-an-array.cpp
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to find leaders in the array
+// Function to find all leaders in the array
 vector<int> leaders(vector<int> &nums)
 {
     vector<int> ans;
+    int n = nums.size();
 
-    // Check every element
-    for (int i = 0; i < nums.size(); i++)
+    // Edge case: if the array is empty
+    if (n == 0)
+        return ans;
+
+    // The last element is always a leader
+    int maxi = nums[n - 1];
+    ans.push_back(maxi);
+
+    // Traverse the array from right to left
+    for (int i = n - 2; i >= 0; i--)
     {
-        bool leader = true;
-
-        // Compare with all elements to its right
-        for (int j = i + 1; j < nums.size(); j++)
-        {
-            if (nums[j] > nums[i])
-            {
-                leader = false;
-                break;
-            }
-        }
-
-        // If still a leader, add to answer
-        if (leader)
+        // If the current element is greater than or equal
+        // to the maximum element seen so far, it is a leader
+        if (nums[i] >= maxi)
         {
             ans.push_back(nums[i]);
+
+            // Update the maximum element
+            maxi = nums[i];
         }
     }
+
+    // Leaders were collected from right to left,
+    // so reverse the answer to restore the original order
+    reverse(ans.begin(), ans.end());
 
     return ans;
 }
@@ -35,13 +40,18 @@ int main()
 {
     vector<int> nums = {1, 2, 5, 3, 1, 2};
 
+    // Find the leaders in the array
     vector<int> ans = leaders(nums);
 
     cout << "Leaders in the array are: ";
+
+    // Print all the leaders
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
     }
+
+    cout << endl;
 
     return 0;
 }
