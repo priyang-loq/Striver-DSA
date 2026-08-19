7a6304c874dada07dc6e984c7c8dda2611ad79d7
Priyangshu Das <priyangshud924@gmail.com>
2026-08-19 21:34:03 +0530

    Refactor rowWithMax1s function to use binary search for improved efficiency and update test matrix

--- 07_BinarySearch/03_BS_On_2D_Arrays/01_find-the-row-with-maximum-number-of-1s.cpp
+++ 07_BinarySearch/03_BS_On_2D_Arrays/01_find-the-row-with-maximum-number-of-1s.cpp
@@ -7,33 +7,55 @@ public:
     int rowWithMax1s(vector<vector<int>> &matrix, int n, int m)
     {
         int index = -1;
-        int prevCount = INT_MIN;
+        int numberofOnes = INT_MIN;
+
         for (int i = 0; i < n; i++)
         {
-            int count = 0;
-            for (int j = 0; j < m; j++)
+            int low = 0;
+            int high = m - 1;
+            int firstOneIndex = m;
+
+            while (low <= high)
             {
-                if (matrix[i][j] == 1)
+                int mid = low + (high - low) / 2;
+
+                if (matrix[i][mid] == 1)
                 {
-                    count++;
+                    firstOneIndex = mid;
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
                 }
             }
-            if (count > prevCount)
+
+            int ones = m - firstOneIndex;
+
+            if (ones > numberofOnes)
             {
-                prevCount = count;
+                numberofOnes = ones;
                 index = i;
             }
         }
+
         return index;
     }
 };
 
 int main()
 {
-    vector<vector<int>> matrix = {{1, 0, 1}, {0, 0, 1}, {1, 1, 1}};
+    vector<vector<int>> matrix = {
+        {0, 1, 1},
+        {0, 0, 1},
+        {1, 1, 1}};
+
     int n = 3, m = 3;
 
     Solution obj;
-    cout << "The row with maximum no. of 1's is: " << obj.rowWithMax1s(matrix, n, m) << '\n';
+
+    cout << "The row with maximum no. of 1's is: "
+         << obj.rowWithMax1s(matrix, n, m) << '\n';
+
     return 0;
 }
