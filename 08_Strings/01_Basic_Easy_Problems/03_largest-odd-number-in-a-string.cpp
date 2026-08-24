74fb7fe1505bf7b92eac4d2d3ab137fe51dde601
Priyangshu Das <priyangshud924@gmail.com>
2026-08-24 20:10:26 +0530

    Refactor largest odd number substring function for clarity and efficiency

--- 08_Strings/01_Basic_Easy_Problems/03_largest-odd-number-in-a-string.cpp
+++ 08_Strings/01_Basic_Easy_Problems/03_largest-odd-number-in-a-string.cpp
@@ -1,34 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
-    // Returns the largest odd number substring from the given string
-    string largeOddNum(string& s) {
-        int ind = -1;
-
-        // Find the last odd digit in the string
-        int i;
-        for (i = s.length() - 1; i >= 0; i--) {
-            if ((s[i] - '0') % 2 == 1) {
-                ind = i;
-                break;
+    string largestOddNumber(string num) {
+        for (int i = num.size() - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 == 1) {
+                return num.substr(0, i + 1);
             }
         }
 
-        // Skip leading zeroes up to the odd digit
-        i = 0;
-        while (i <= ind && s[i] == '0') i++;
-
-        // Return substring from first non-zero to odd digit
-        return s.substr(i, ind - i + 1);
+        return "";
     }
 };
 
 int main() {
     Solution solution;
     string num = "504";
-    string result = solution.largeOddNum(num);
+    string result = solution.largestOddNumber(num);
     cout << "Largest odd number: " << result << endl;
     return 0;
 }
